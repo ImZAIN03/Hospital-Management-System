52e85056dcfaf24ed7825d1ea0b5f91e82d22932
Muhammed Zain <muhammedzain2256@gmail.com>
2024-07-17 13:33:08 +0500

    AboutUs Page

--- Frontend/src/pages/AboutUs.jsx
+++ Frontend/src/pages/AboutUs.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../components/Hero";
+import Biography from "../components/Biography";
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div style={{ paddingTop: '4%'}}>
+      <Hero 
+        title={"Learn More About Us | HMS Medical Institute"}
+        imageUrl={"/about.png"}
+      />
+      <Biography imageUrl={"/whoweare.png"} />
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
